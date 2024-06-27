d208c65275f4906df92ae3081219c740e897e5ef
habioye <78454514+habioye@users.noreply.github.com>
2024-06-27 13:26:52 -0500

    connected the input to a global variable

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { NgForm, FormsModule } from '@angular/forms';
+// import { Guid } from "guid-typescript";
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  current = 0;
+  inputValue = "";
+  onSubmit(input: NgForm)  {
+    // let todo = new Todo(Guid.create(), input.value.title, false);
+    // this.todos.push(todo);
 
+
+  }
 }
