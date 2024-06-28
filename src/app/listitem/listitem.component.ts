2e983a65a01b08daa9d3e20cf33a563b7eaeeeaa
habioye <78454514+habioye@users.noreply.github.com>
2024-06-28 07:44:36 -0500

    added the list item component

---
+++ src/app/listitem/listitem.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-listitem',
+  templateUrl: './listitem.component.html',
+  styleUrl: './listitem.component.css'
+})
+export class ListitemComponent {
+    @Input() item: {name: string };
+    
+}
